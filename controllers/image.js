478f6e511900dfbf2970f426865e3599deab112f
Laura <laura.stasiule@gmail.com>
2020-05-23 15:13:39 +0300

    Add initial files

---
+++ controllers/image.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({ apiKey: process.env.CLARIFAI_API_KEY });
+
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => res.json(data))
+        .catch(err => res.status(400).json('unable to work with API'));
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries);
+        })
+        .catch(err => res.status(400).json('error getting entries'));
+}
+
+module.exports = {
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
+}
