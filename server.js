f3f93730b3ac5c2e28fa97f8b6b9d8dfff29fb2d
Laura <laura.stasiule@gmail.com>
2020-05-23 20:03:48 +0300

    Update database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1', //localhost
-        user: process.env.PG_USER,
-        password: process.env.PG_PASSWORD,
-        database: 'smart_brain'
+        host: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
